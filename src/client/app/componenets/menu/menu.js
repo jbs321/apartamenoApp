0ef3840056c6aeb9bc3adf17770d9277136c09e6
jacob <jbs321.work@gmail.com>
2017-06-13 22:59:17 -0700

    add the compiled css

--- src/client/app/componenets/menu/menu.js
+++ src/client/app/componenets/menu/menu.js
@@ -8,19 +8,19 @@ class Menu extends Component {
 
     render() {
         return (
-            <nav className=" menu navbar navbar-toggleable-md navbar-light bg-faded">
+            <nav className="menu navbar navbar-toggleable-md navbar-light bg-faded d-flex justify-content-end">
                 <ul className="navbar-nav mr-auto">
-                    <li className="nav-item my-lg-0">
+                    <li className="nav-item">
                         <img className="icon" src="./public/img/icon.png" alt=""/>
                     </li>
 
                     <li className="nav-item">
                         <a className="nav-link active" href="#" onClick={this.handleNav}>Home</a>
                     </li>
-                    <li className="nav-item">
+                    <li className="nav-item ">
                         <a className="nav-link" href="#" onClick={this.handleNav}>Buildings</a>
                     </li>
-                    <li className="nav-item">
+                    <li className="nav-item ml-auto p-2">
                         <a className="nav-link" href="#" onClick={this.handleNav}>Users</a>
                     </li>
                 </ul>
