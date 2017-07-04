483039ca83c50b2ee1d009c95c40aea949a5d63f
JB <jacob.lanir@grouphealth.ca>
2017-07-04 16:48:00 -0700

    add a drawer

--- src/client/app/componenets/header/header.jsx
+++ src/client/app/componenets/header/header.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import AppBar from 'material-ui/AppBar';
 import Search from "../search.jsx";
+import DrawerSimpleExample from "../DrawerSimpleExample.jsx";
 
 class Header extends Component {
 
@@ -13,6 +14,7 @@ class Header extends Component {
                     <h1 className="col-xs-12 col-sm-10 col-md-10 col-lg-7">Apartamento</h1>
                     <h2 className="col-xs-12 col-sm-10 col-md-10 col-lg-7">Find all you need about Apartments, Buildings and more.</h2>
                     <Search buildings={this.props.buildings}/>
+                    <DrawerSimpleExample />
                 </div>
             </header>
         );
