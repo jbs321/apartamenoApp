7ba37d2f3bc61f64c1814170bcc015d1615f4ca9
JB <jacob.lanir@grouphealth.ca>
2017-07-05 16:56:55 -0700

    add loading image

--- src/client/app/componenets/building/building.jsx
+++ src/client/app/componenets/building/building.jsx
@@ -2,32 +2,34 @@ import React from 'react';
 import Ratings from '../rating/ratings.jsx'
 import Comment from '../comment/comment.jsx'
 
-class Building extends React.Component {
+export default class Building extends React.Component {
     render() {
 
         let building = this.props.buildings.filter((obj) => obj.id == this.props.match.params.id)[0];
 
-        return (
-            <div className="container-fluid no-pad building-page">
-                <div className="row">
-                    <div className="col-12">
-                        <img src={building.imgSrc} className="img-responsive"/>
-                    </div>
-                    <div className="col-12">
-                        <h3 className="address">{building.address}</h3>
-                    </div>
+        if(building == undefined) {
+            return (<div>Nothing here</div>)
+        } else {
+            return (
+                <div className="container-fluid no-pad building-page">
+                    <div className="row">
+                        <div className="col-12">
+                            <img src={building.imgSrc} className="img-responsive"/>
+                        </div>
+                        <div className="col-12">
+                            <h3 className="address">{building.address}</h3>
+                        </div>
 
-                    <div className="col-12 ratings-wrapper">
-                        <Ratings ratings={building.ratings}/>
-                    </div>
+                        <div className="col-12 ratings-wrapper">
+                            <Ratings ratings={building.ratings}/>
+                        </div>
 
-                    <div className="col-12 comments-wrapper">
-                        <Comment comments={building.comments}/>
+                        <div className="col-12 comments-wrapper">
+                            <Comment comments={building.comments}/>
+                        </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        }
     }
 }
-
-export default Building;
